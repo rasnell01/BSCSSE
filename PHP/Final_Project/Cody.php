9189fd408c19b48f4e751355e9f15be49d06f750
Ryan Snell <43451680+rasnell01@users.noreply.github.com>
2025-03-20 13:48:45 -0400

    Added these to a different directory.

---
+++ PHP/Final_Project/Cody.php
@@ -0,0 +1,53 @@
+<?php
+    define("TITLE", "Meet Cody");
+?>
+
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title><?php echo TITLE; ?>y</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../CSS/styles.css" type="text/css">
+        
+        <!-- file links -->
+        <link href="Index.html">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+            <div>
+                <ul>
+                    <li class="headerMenu"><a href="../PHP/home.php">Home</a></li>
+                    <li class="headerMenu"><a href="Miguel.php">Meet Miguel</a></li>
+                    <li class="headerMenu"><a href="Josh.php">Meet Josh</a></li>
+                    <li class="headerMenu"><a href="Ryan.php">Meet Ryan</a></li>
+                </ul>
+            </div>
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+            <div class="copyright">
+                <h6>Team whateva whateva &copy; 2019.</h6>
+            </div>
+        </footer>
+    </body>
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+        
+        <!-- Bootstrap JS -->
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</html>
---
+++ PHP/Final_Project/finalProject.php
@@ -0,0 +1,36 @@
+<?php
+    
+    define("headerData", "
+    <header>
+        <div>
+            <ul>
+                <li class="headerMenu"><a href="../finalProject.php">Home Page</a></li>
+                <li class="headerMenu"><a href="Miguel.php">Meet Miguel</a></li>
+                <li class="headerMenu"><a href="Josh.php">Meet Josh</a></li>
+                <li class="headerMenu"><a href="Cody.php">Meet Cody</a></li>
+            </ul>
+        </div>
+    </header>")
+    define("footerData","
+    <footer>
+        <div>
+            <ul class="footerMenu">
+                <li class="footerMenu"><a href="finalProject.php">Home</a></li>
+                <li class="footerMenu"><a href="contact.php">Contact</a></li>
+                <li class="footerMenu"><a href="about.php">About</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="subscribe">
+                <li class="subscribe"><a href="https://www.facebook.com/sharer/sharer.php?u=Final_Project_Whateva_Whateva" target="_blank">Share on Facebook</a></li>
+                <li class="subscribe"><a href="https://twitter.com/home?status=Final_Project_Whateva_Whateva" target="_blank">Share on Twitter</a></li>
+                <li class="subscribe"><a href="https://www.instagram.com/home?status=Final_Project_Whateva_Whatevan" target="_blank">Share on Instagram</a></li>
+                <li class="subscribe"><a href="https://www.snapchat.com/?status=Final_Project_Whateva_Whateva" target="_blank">Share on SnapChat</a></li>
+            </ul>
+        </div>
+        <div class="copyright">
+            <h6>Team whateva whateva &copy; 2019.</h6>
+        </div>
+    </footer>")
+?>
+
